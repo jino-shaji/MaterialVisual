082f46a012869ccbed64020c92c2f107708e212c
JINO SHAJI <jinoshajiv@gmail.com>
2020-06-20 21:20:10 +0530

    Add project files.

---
+++ MaterialVisual/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using MaterialVisual.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MaterialVisual
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ MaterialVisual/Views/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MaterialVisual.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public ICommand NavigateCommand { get; private set; }
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigateCommand = new Command<Type>(
+               async (Type pageType) =>
+               {
+                   Page page = (Page)Activator.CreateInstance(pageType);
+                   await Navigation.PushAsync(page);
+               });
+            BindingContext = this;
+        }
+    }
+}
